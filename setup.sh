25cab9d08d1b3de6d3632ce53a0de8a502a4a642
user42 <ljurdant@student.42.fr>
2020-10-14 01:48:38 +0200

    So it begins

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+GREEN="\e[92m"
+YELLOW="\e[93m"
+WHITE="\e[97m"
+
+echo -e $WHITE"Updating kubectl..."
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &> /dev/null
+chmod +x ./kubectl &> /dev/null
+sudo mv ./kubectl /usr/local/bin/kubectl -p 'user42' &> /dev/null
+echo -e $GREEN"Kubectl updated"
+
+echo -e $WHITE"Updating minikube..."
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 &> /dev/null
+chmod +x minikube &> /dev/null
+sudo mv ./minikube /usr/local/bin/minikube &> /dev/null
+sudo chown -R $USER /usr/local/bin/minikube; chmod -R u+wrx /usr/local/bin/minikube
+sudo apt-get install -y conntrack &> /dev/null
+sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube &> /dev/null
+echo -e $GREEN"Minikube updated"
+
+#echo -e $WHITE"Setting docker user..."
+#touch ~/.vm_setup
+#sudo usermod -aG docker user42 && newgrp docker
+#echo -e $GREEN"Docker user set"
+
+echo -e $WHITE"Starting minikube..."
+minikube delete &> /dev/null
+minikube start --driver=docker &> /dev/null
+echo -e $GREEN"Minikube started"
+
+echo -e $WHITE
+minikube addons enable metrics-server
+minikube addons enable dashboard
+minikube addons enable metallb
+
+echo "Wordpress build"
+docker build ./Wordpress --tag wordpress:latest
+kubectl create deployment wordpress-depl --image=wordpress:latest --port=80
