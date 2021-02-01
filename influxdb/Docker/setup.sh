0745732ff2e34cdf8603a6618a5ab54efc3c3a09
user42 <ljurdant@student.42.fr>
2021-02-01 18:46:41 +0100

    fixed influxdb and telegraf

---
+++ influxdb/Docker/setup.sh
@@ -0,0 +1,13 @@
+# Start telegraf
+/telegraf-1.17.2/usr/bin/telegraf &
+
+# Start influxdb
+/usr/sbin/influxd & sleep 3
+
+# Initialize database
+influx -execute "CREATE DATABASE grafana"
+influx -execute "CREATE USER ljurdant WITH PASSWORD 'yo'"
+influx -execute "GRANT ALL ON grafana TO ljurdant"
+
+# Keep container running
+tail -f /dev/null
