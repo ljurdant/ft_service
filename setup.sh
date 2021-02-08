81d648828de4a7f5382f9204fcfa0b9031ebf62a
user42 <ljurdant@student.42.fr>
2021-02-08 16:18:37 +0100

    put everything in srcs

--- setup.sh
+++ setup.sh
@@ -15,51 +15,51 @@ eval $(minikube docker-env) &> /dev/null
 kubectl delete configmap config -n metallb-system &> /dev/null
 
 echo "Building docker images..."
-docker build ./wordpress/Docker --tag=wordpress:latest &> /dev/null
-docker build ./nginx/Docker --tag=nginx:latest &> /dev/null
-docker build ./mysql/Docker --tag=mysql:latest &> /dev/null
-docker build ./phpmyadmin/Docker --tag=phpmyadmin:latest &> /dev/null
-docker build ./ftps/Docker --tag=ftps:latest &> /dev/null
-docker build ./influxdb/Docker --tag=influxdb:latest &> /dev/null
-docker build ./grafana/Docker --tag=grafana:latest &> /dev/null
-docker build ./telegraf/Docker --tag=telegraf:latest &> /dev/null
+docker build ./srcs/wordpress/Docker --tag=wordpress:latest &> /dev/null
+docker build ./srcs/nginx/Docker --tag=nginx:latest &> /dev/null
+docker build ./srcs/mysql/Docker --tag=mysql:latest &> /dev/null
+docker build ./srcs/phpmyadmin/Docker --tag=phpmyadmin:latest &> /dev/null
+docker build ./srcs/ftps/Docker --tag=ftps:latest &> /dev/null
+docker build ./srcs/influxdb/Docker --tag=influxdb:latest &> /dev/null
+docker build ./srcs/grafana/Docker --tag=grafana:latest &> /dev/null
+docker build ./srcs/telegraf/Docker --tag=telegraf:latest &> /dev/null
 
 echo "Configuring metallb..."
-kubectl apply -f metallb/metallb.yaml &> /dev/null
+kubectl apply -f srcs/metallb/metallb.yaml &> /dev/null
 
 echo "Launching nginx service..."
-kubectl apply -f nginx/nginx-deployment.yaml &> /dev/null
-kubectl apply -f nginx/nginx-service.yaml &> /dev/null
+kubectl apply -f srcs/nginx/nginx-deployment.yaml &> /dev/null
+kubectl apply -f srcs/nginx/nginx-service.yaml &> /dev/null
 
 echo "Launching mysql service..."
-kubectl apply -f mysql/mysql-pvc.yaml &> /dev/null
-kubectl apply -f mysql/mysql-deployment.yaml &> /dev/null
-kubectl apply -f mysql/mysql-service.yaml &> /dev/null
+kubectl apply -f srcs/mysql/mysql-pvc.yaml &> /dev/null
+kubectl apply -f srcs/mysql/mysql-deployment.yaml &> /dev/null
+kubectl apply -f srcs/mysql/mysql-service.yaml &> /dev/null
 
 echo "Launching wordpress service..."
-kubectl apply -f wordpress/wordpress-deployment.yaml &> /dev/null
-kubectl apply -f wordpress/wordpress-service.yaml &> /dev/null
+kubectl apply -f srcs/wordpress/wordpress-deployment.yaml &> /dev/null
+kubectl apply -f srcs/wordpress/wordpress-service.yaml &> /dev/null
 
 echo "Launching phpmyadmin service..."
-kubectl apply -f phpmyadmin/phpmyadmin-deployment.yaml &> /dev/null
-kubectl apply -f phpmyadmin/phpmyadmin-service.yaml &> /dev/null
+kubectl apply -f srcs/phpmyadmin/phpmyadmin-deployment.yaml &> /dev/null
+kubectl apply -f srcs/phpmyadmin/phpmyadmin-service.yaml &> /dev/null
 
 echo "Launching ftps service..."
-kubectl apply -f ftps/ftps-deployment.yaml &> /dev/null
-kubectl apply -f ftps/ftps-service.yaml &> /dev/null
+kubectl apply -f srcs/ftps/ftps-deployment.yaml &> /dev/null
+kubectl apply -f srcs/ftps/ftps-service.yaml &> /dev/null
 
 echo "Launching influxdb service..."
-kubectl apply -f influxdb/influxdb-volume.yaml &> /dev/null
-kubectl apply -f influxdb/influxdb-deployment.yaml &> /dev/null
-kubectl apply -f influxdb/influxdb-service.yaml &> /dev/null
+kubectl apply -f srcs/influxdb/influxdb-volume.yaml &> /dev/null
+kubectl apply -f srcs/influxdb/influxdb-deployment.yaml &> /dev/null
+kubectl apply -f srcs/influxdb/influxdb-service.yaml &> /dev/null
 
 echo "Launching grafana service..."
-kubectl apply -f grafana/grafana-deployment.yaml &> /dev/null
-kubectl apply -f grafana/grafana-service.yaml &> /dev/null
-kubectl apply -f telegraf/telegraf-secret.yaml &> /dev/null
-kubectl apply -f telegraf/telegraf-config.yaml &> /dev/null
-kubectl apply -f telegraf/telegraf-deployment.yaml &> /dev/null
-kubectl apply -f telegraf/telegraf-service.yaml &> /dev/null
+kubectl apply -f srcs/grafana/grafana-deployment.yaml &> /dev/null
+kubectl apply -f srcs/grafana/grafana-service.yaml &> /dev/null
+kubectl apply -f srcs/telegraf/telegraf-secret.yaml &> /dev/null
+kubectl apply -f srcs/telegraf/telegraf-config.yaml &> /dev/null
+kubectl apply -f srcs/telegraf/telegraf-deployment.yaml &> /dev/null
+kubectl apply -f srcs/telegraf/telegraf-service.yaml &> /dev/null
 
 echo -en $GREEN && echo "ft_services is ready!"
 echo "Go to IP 192.168.49.240 to try it!"
