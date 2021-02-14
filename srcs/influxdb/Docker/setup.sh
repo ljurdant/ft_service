a5b69341b1e22711da0707d4a1aeb68b23d6f083
ljurdant <ljurdant@student.42.fr>
2021-02-14 13:53:37 +0100

    corrected for influxdb restart

--- srcs/influxdb/Docker/setup.sh
+++ srcs/influxdb/Docker/setup.sh
@@ -1,10 +1,14 @@
-# Start influxdb
+# Star luxdb
 /usr/sbin/influxd & sleep 3
 
-# Initialize database
+mkdir -p /var/lib/influxdb/data
+mkdir -p /var/lib/influxdb/wal
+mkdir -p /var/lib/influxdb/meta
+#Initialize database
 influx -execute "CREATE DATABASE grafana"
 influx -execute "CREATE USER ljurdant WITH PASSWORD 'yo'"
 influx -execute "GRANT ALL ON grafana TO ljurdant"
+
 
 # Keep container running
 tail -f /dev/null
--- srcs/mysql/Docker/init_db.sh
+++ srcs/mysql/Docker/init_db.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-rc-update add mariadb default
 if [ ! -d /var/lib/mysql/wordpress ]
 then
 	(
-	mysql_install_db --user=mysql --datadir=${DB_DATA_PATH}
-	rc-update add mariadb default
-	rc boot && rc-service mariadb start 
-	mysql --execute="create database wordpress default character set utf8 collate utf8_unicode_ci" 
-	mysql --execute="grant all on *.* to 'root'@'%' identified by 'root'"
+	/etc/init.d/mariadb setup 
+ 	service mariadb start
+	mysql --execute "CREATE DATABASE wordpress" 
 	mysql --execute "GRANT ALL PRIVILEGES ON wordpress.* TO 'ljurdant'@'%' IDENTIFIED BY 'yo' ;"
-	mysql --execute "GRANT INSERT ON wordpress.* TO 'bobby'@'%' IDENTIFIED BY 'yo' ;" | mysql -u root \
-	mysql --execute "flush privileges" 
-	mysql -uroot -proot wordpress < wordpress.sql
+	mysql --execute "GRANT INSERT ON wordpress.* TO 'bobby'@'%' IDENTIFIED BY 'yo' ;"
+	mysql --execute "FLUSH PRIVILEGES;" 
+	mysql -u root wordpress < wordpress.sql
 	)
 else
-	mysql -uroot -proot wordpress < wp_users.sql
-	rc boot && rc-service mariadb start
-fi	
-rc-service mariadb restart 
-sleep infinity
+ 	echo "hellooooooooooooooooooooooooooooooooOOOOOOOOOOOOO\n"
+	mysql -u root wordpress < wp_users.sql
+	service mariadb start
+fi
+sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf	
+service mariadb restart
+tail -f /dev/null
