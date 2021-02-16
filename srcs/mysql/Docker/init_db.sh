f31282c8d6feec072ee96b8d38a2dfd4b9bc1ae5
ljurdant <ljurdant@student.42.fr>
2021-02-16 10:06:49 +0100

    fixed mysql

--- srcs/mysql/Docker/init_db.sh
+++ srcs/mysql/Docker/init_db.sh
@@ -4,6 +4,7 @@ if [ ! -d /var/lib/mysql/wordpress ]
 then
 	(
 	/etc/init.d/mariadb setup 
+	sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf	
  	service mariadb start
 	mysql --execute "CREATE DATABASE wordpress" 
 	mysql --execute "GRANT ALL PRIVILEGES ON wordpress.* TO 'ljurdant'@'%' IDENTIFIED BY 'yo' ;"
@@ -12,10 +13,12 @@ then
 	mysql -u root wordpress < wordpress.sql
 	)
 else
- 	echo "hellooooooooooooooooooooooooooooooooOOOOOOOOOOOOO\n"
-	mysql -u root wordpress < wp_users.sql
+ 	#echo "hellooooooooooooooooooooooooooooooooOOOOOOOOOOOOO\n"
+	/etc/init.d/mariadb setup 
+	sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf	
 	service mariadb start
+	#mysql -u root wordpress < wp_users.sql
 fi
-sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf	
-service mariadb restart
+#sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf	
+#service mariadb restart
 tail -f /dev/null
